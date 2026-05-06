dad3b56f2f8d264a4ca36c8a95a050e5a6e44f08
deleo <deleo@192.168.1.43>
2026-05-05 21:26:57 -0600

    subiendo pantalla

---
+++ src/main/java/com/mycompany/reporte1/Reporte1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.reporte1;
+
+/**
+ *
+ * @author deleo
+ */
+public class Reporte1 {
+
+    public static void main(String[] args) {
+     java.awt.EventQueue.invokeLater(() -> {
+            // Crea y hace visible la ventana de tu reporte
+            new General1().setVisible(true);
+        });
+    }
+}
