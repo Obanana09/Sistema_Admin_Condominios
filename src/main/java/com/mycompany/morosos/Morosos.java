d2c783c776429991d3c151287ed9b4261f7bc261
deleo <deleo@192.168.1.43>
2026-05-17 20:59:44 -0600

    subiendo pantalla de casas morosas

---
+++ src/main/java/com/mycompany/morosos/Morosos.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.morosos;
+
+/**
+ *
+ * @author deleo
+ */
+public class Morosos {
+
+    public static void main(String[] args) {
+       java.awt.EventQueue.invokeLater(() -> {
+            new CasasM().setVisible(true);
+        });
+    }
+}
